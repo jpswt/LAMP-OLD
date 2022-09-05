faa82106a28a786495b8068f893111f78b6dbf09
jpswt <joel_swt@yahoo.com>
2022-09-05 16:13:04 -0500

    cookie

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -78,7 +78,6 @@ const login = async (req, res) => {
 				let signedToken = jwt.sign(token, process.env.JWT_SECRET);
 				res.cookie('token', signedToken, { httpOnly: true });
 				res.send(signedToken);
-				console.log(signedToken);
 				// res.cookie('access-token', signedToken, {
 				// 	maxAge: 60 * 60 * 24 * 30 * 1000,
 				// });
