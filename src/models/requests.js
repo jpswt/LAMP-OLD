8935fa7c7a822934bd14964df11b9400750a3daa
jpswt <joel_swt@yahoo.com>
2022-09-10 19:03:21 -0500

    changes to send request

--- src/models/requests.js
+++ src/models/requests.js
@@ -2,13 +2,13 @@ const helper = require('../helper/promisify');
 
 const sentVolRequests = (userName) => {
 	let sql =
-		'select org_id, users1.name, tasks.type, tasks.description, start_time, time_span, message, isAccepted, created_on from request join users1 on request.vol_username = users1.username join tasks on tasks.id = request.task_code where vol_username = ? order by created_on desc';
+		'select org_id, users1.name, start_time, time_span, message, created_on, isAccepted from request join users1 on request.vol_username = users1.username where vol_username = ? order by created_on desc';
 	return helper.promisify(sql, userName);
 };
 
 const receivedOrgRequests = (userId) => {
 	let sql =
-		'select request.id, users1.username, users1.name, users1.email, tasks.type, tasks.description, start_time, message, time_span, isAccepted, created_on from request join users1 on request.vol_username = users1.username join tasks on tasks.id = request.task_code where org_id = ? order by created_on desc';
+		'select request.id, users1.username, users1.name, users1.email, tasks.type, tasks.description, start_time, message, time_span, created_on, isAccepted from request join users1 on request.vol_username = users1.username where org_id = ? order by created_on desc';
 	return helper.promisify(sql, userId);
 };
 
